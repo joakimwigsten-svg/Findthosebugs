84de63bd75ab9a05dd81928aeaa664a1eb0894e5
Joakim <joakim.wigsten@gmail.com>
2026-09-15 20:05:22 +0200

    Added Testfile for todostats

---
+++ src/components/tests/TodoStats.test.tsx
@@ -0,0 +1,12 @@
+import {describe, it} from "vitest";
+
+describe("TodoStats", () => {
+    it("should render the correct number of tasks", () => {
+        // arrange
+        const tasks = [
+            {id: 1, text: "Task 1", completed: false},
+            {id: 2, text: "Task 2", completed: true},
+            {id: 3, text: "Task 3", completed: false}
+        ]
+});
+});
